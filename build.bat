5d9d7af231e5103b9804a8effb0132a0a0af7036
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2023-12-29 01:43:25 +0530

    renamed the output executable file

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
+echo "Activating Virtual Environment"
 call ./venv/Scripts/activate.bat
-call pyinstaller main.py --onefile --name Table
+
+echo "Creating executable file"
+call pyinstaller main.py --onefile --name "PhysicsPracticalTable_by_P.Sekhar"
+
+echo "Output file is saved in /dist folder"
+
+echo 'Deactivating Virtual Environment'
 call ./venv/Scripts/deactivate.bat
--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 echo "Activating Virtual Environment"
 .\venv\Scripts\Activate.ps1
 echo "Creating executable file"
-pyinstaller main.py --onefile --name Table
+pyinstaller main.py --onefile --name "PhysicsPracticalTable_by_P.Sekhar"
 echo "Output file is saved in /dist folder"
 echo 'Deactivating Virtual Environment'
 deactivate
