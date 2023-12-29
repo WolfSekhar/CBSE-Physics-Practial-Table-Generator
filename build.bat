672690659fe3b6044bef2d71c6a4da74bb84c6db
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2023-12-30 01:18:34 +0530

    added icon program

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ echo "Activating Virtual Environment"
 call ./venv/Scripts/activate.bat
 
 echo "Creating executable file"
-call pyinstaller main.py --onefile --name "PhysicsPracticalTable_by_P.Sekhar"
+call pyinstaller main.py --onefile --name "PhysicsPracticalTable_by_P.Sekhar" --icon=./icon.ico
 
 echo "Output file is saved in /dist folder"
 
--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 echo "Activating Virtual Environment"
 .\venv\Scripts\Activate.ps1
 echo "Creating executable file"
-pyinstaller main.py --onefile --name "PhysicsPracticalTable_by_P.Sekhar" --clean
+pyinstaller main.py --onefile --name "PhysicsPracticalTable_by_P.Sekhar" --clean --icon=.\icon.ico
 echo "Output file is saved in /dist folder"
 echo 'Deactivating Virtual Environment'
 deactivate
