55d28ea291898747ace59125a381777488922bab
WolfSekhar <64012012+WolfSekhar@users.noreply.github.com>
2024-01-17 08:30:42 +0530

    reformat code

--- main.py
+++ main.py
@@ -8,79 +8,75 @@ import xii.galvanometer_to_voltmeter as GTV
 import xii.concave_mirror_focal_length as CMF
 import art as art
 
+
 class Main():
-	def __init__(self):
-		self.experiments = {}
-		self.console = Console()
-		self.number_of_experiment = 0
-		self.importAllExperimentsWithPath()
+    def __init__(self):
+        self.experiments = {}
+        self.console = Console()
+        self.number_of_experiment = 0
+        self.importAllExperimentsWithPath()
 
-	def importAllExperimentsWithPath(self):
-		self.experiments[1] = ("To determine resistivity of two / three wires " +
-		 " by plotting a graph for potential difference versus current.")
-		self.experiments[2] = (("To find resistance of a given wire / standard resistor using metre bridge."))
-		self.experiments[3] =("To verify the laws of combination (series) of resistances using a metre bridge.")
-		self.experiments[4] = ("To verify the laws of combination (parallel) of resistances using a metre bridge.")
-		self.experiments[5] = ('To determine resistance of a galvanometer by half-deflection method and to find its'+
-			'figure of merit')
-		self.experiments[6] = ("To convert the given galvanometer (of known resistance and figure of merit) into a " +
-			"voltmeter of desired range and to verify.")
-		self.experiments[7] = ("To find the value of v for different values of u in case of a concave mirror and to find the focal length.")
+    def importAllExperimentsWithPath(self):
+        self.experiments[1] = ("To determine resistivity of two / three wires " +
+                               " by plotting a graph for potential difference versus current.")
+        self.experiments[2] = (("To find resistance of a given wire / standard resistor using metre bridge."))
+        self.experiments[3] = ("To verify the laws of combination (series) of resistances using a metre bridge.")
+        self.experiments[4] = ("To verify the laws of combination (parallel) of resistances using a metre bridge.")
+        self.experiments[5] = ('To determine resistance of a galvanometer by half-deflection method and to find its' +
+                               'figure of merit')
+        self.experiments[6] = ("To convert the given galvanometer (of known resistance and figure of merit) into a " +
+                               "voltmeter of desired range and to verify.")
+        self.experiments[7] = (
+            "To find the value of v for different values of u in case of a concave mirror and to find the focal length.")
 
+    def showOptions(self):
+        self.console.print("Choose Experiment : ", style="red")
+        for exp in self.experiments:
+            self.console.print(str(exp) + " : ", end='')
+            self.console.print("[#af8787]" + self.experiments.get(exp))
 
-	def showOptions(self):
-		self.console.print("Choose Experiment : ", style="red")
-		for exp in self.experiments:
-			self.console.print(str(exp) + " : ",end= '')
-			self.console.print("[#af8787]"+self.experiments.get(exp))
-	def takeInput(self):
-		input_experiment_number = input("Enter the experiment number : ")
-		
-		try: 
-			self.number_of_experiment = int(input_experiment_number)
-			self.console.print(input_experiment_number)
-		except ValueError:
-			self.console.print("ERROR")
-		
+    def takeInput(self):
+        input_experiment_number = input("Enter the experiment number : ")
 
-	def showTabulation(self,experiment_number):
-		if experiment_number == 3:
-			Series.Series().run()
-		elif experiment_number == 4:
-			Parallel.Parallel().run()
-		elif experiment_number == 2:
-			ResistanceOfWire.ResistanceOfWire().run()
-		elif experiment_number == 1:
-			ResistivityOfWire.ResistivityOfWires().run()
-		elif experiment_number == 5:
-			GHalfDeflection.GalvanometerHalfDeflection().run()
-		elif experiment_number == 6:
-			GTV.GalvanometerToVoltmeter().run()
-		elif experiment_number == 7:
-			CMF.ConcaveMirror().run()
-		else:
-			exit()
+        try:
+            self.number_of_experiment = int(input_experiment_number)
+            self.console.print(input_experiment_number)
+        except ValueError:
+            self.console.print("ERROR")
 
+    def showTabulation(self, experiment_number):
+        if experiment_number == 3:
+            Series.Series().run()
+        elif experiment_number == 4:
+            Parallel.Parallel().run()
+        elif experiment_number == 2:
+            ResistanceOfWire.ResistanceOfWire().run()
+        elif experiment_number == 1:
+            ResistivityOfWire.ResistivityOfWires().run()
+        elif experiment_number == 5:
+            GHalfDeflection.GalvanometerHalfDeflection().run()
+        elif experiment_number == 6:
+            GTV.GalvanometerToVoltmeter().run()
+        elif experiment_number == 7:
+            CMF.ConcaveMirror().run()
+        else:
+            exit()
 
-	def test(self):
-		for i in range(2):
-			s = Series.Series()
-			s.run()
+    def test(self):
+        for i in range(2):
+            s = Series.Series()
+            s.run()
 
-	def closeProgramAlert(self):
-		self.console.print("\n\n [bold red] PRESS ENTER TO CLOSE THE PROGRAM : ")
+    def closeProgramAlert(self):
+        self.console.print("\n\n [bold red] PRESS ENTER TO CLOSE THE PROGRAM : ")
 
-	def showAuthor(self):
-		self.console.print("[bold bright_magenta]PHYSICS PRACTICAL TABLE GENERATOR",justify = "center")
-		self.console.print(art.text2art("PHY - P - GEN",chr_ignore=False))
-		self.console.print("[b] By : [b blue]P.SEKHAR [white]([#87d700 b]PGT PHYSICS[white])",justify = "center")
-		self.console.print("[b]contact : [b #5f87d7] sekhar.root@gmail.com",justify = "center")
-		self.console.print("[b] url : [b #00ff00]https://github.com/WolfSekhar",justify = "center")
-		self.console.print("Version : 1.0.1",justify= "center")
-
-
-
-
+    def showAuthor(self):
+        self.console.print("[bold bright_magenta]PHYSICS PRACTICAL TABLE GENERATOR", justify="center")
+        self.console.print(art.text2art("PHY - P - GEN", chr_ignore=False))
+        self.console.print("[b] By : [b blue]P.SEKHAR [white]([#87d700 b]PGT PHYSICS[white])", justify="center")
+        self.console.print("[b]contact : [b #5f87d7] sekhar.root@gmail.com", justify="center")
+        self.console.print("[b] url : [b #00ff00]https://github.com/WolfSekhar", justify="center")
+        self.console.print("Version : 1.0.1", justify="center")
 
 
 main = Main()
--- xi/diameter_wire.py
+++ xi/diameter_wire.py
@@ -3,75 +3,74 @@ from rich.table import Table
 import random as random
 import matplotlib.pyplot as plt
 
+
 class DiameterOfWire:
-	def __init__(self):
-		pass
-	def run(self):
-		console = Console(record = True)
+    def __init__(self):
+        pass
 
-		# diameter table
+    def run(self):
+        console = Console(record=True)
 
-		table2 = Table(title= '[bold white on yellow]Diameter of wire',show_lines = True)
+        # diameter table
 
-		table2.add_column('[red]No of readings',style='red')
-		table2.add_column('LSR')
-		table2.add_column('NCR')
-		table2.add_column('CSC')
-		table2.add_column('CSR =  CSC x LC')
-		table2.add_column('diameter')
+        table2 = Table(title='[bold white on yellow]Diameter of wire', show_lines=True)
 
-		pitch = 0.1
-		LC = 0.001
-		LSR = []
-		NCR = []
-		CSC = []
-		CSR = []
-		diameter = []
-		meanDiameter = [0.081]
-		experiment_number = []
+        table2.add_column('[red]No of readings', style='red')
+        table2.add_column('LSR')
+        table2.add_column('NCR')
+        table2.add_column('CSC')
+        table2.add_column('CSR =  CSC x LC')
+        table2.add_column('diameter')
 
+        pitch = 0.1
+        LC = 0.001
+        LSR = []
+        NCR = []
+        CSC = []
+        CSR = []
+        diameter = []
+        meanDiameter = [0.081]
+        experiment_number = []
 
+        # random increment or decrement value
+        def stepValue(value):
+            tempValue = random.randrange(0, 3)
+            if (tempValue == 0):
+                return value
+            elif (tempValue == 1):
+                return -value
+            else:
+                return 0
 
-		# random increment or decrement value
-		def stepValue(value):
-			tempValue = random.randrange(0,3)
-			if(tempValue == 0):
-				return value
-			elif(tempValue == 1):
-				return -value
-			else:
-				return 0
+        # diameter
 
-		
-		# diameter
+        experiment_number.clear()
 
-		experiment_number.clear()
+        for d in meanDiameter:
+            for i in range(5):
+                experiment_number.append(i + 1)
+                tempDiameter = round(d + stepValue(0.001), 3)
+                diameter.append(tempDiameter)
+                CSR.append(tempDiameter)
+                tempCSC = round(tempDiameter / LC)
+                CSC.append(tempCSC)
+                NCR.append(0)
+                LSR.append(0)
 
-		for d in meanDiameter:
-			for i in range(5):
-				experiment_number.append(i + 1)
-				tempDiameter = round(d + stepValue(0.001),3)
-				diameter.append(tempDiameter)
-				CSR.append(tempDiameter)
-				tempCSC = round(tempDiameter / LC)
-				CSC.append(tempCSC)
-				NCR.append(0)
-				LSR.append(0)
+        for i in range(5):
+            table2.add_row(str(experiment_number[i]), str(LSR[i]), str(NCR[i]), str(CSC[i]), str(CSR[i]),
+                           str(diameter[i]))
 
+        print('\n')
 
-		for i in range(5):
-			table2.add_row(str(experiment_number[i]),str(LSR[i]),str(NCR[i]),str(CSC[i]),str(CSR[i]),str(diameter[i]))
+        console.print(table2)
 
-		print('\n')
+        lengthOfDiameters = len(diameter)
+        first_wire_diameters = diameter[0:lengthOfDiameters // 2]
 
-		console.print(table2)
+        meanDiameterOfFirstWire = round(sum(first_wire_diameters) / (lengthOfDiameters // 2), 3)
 
-		lengthOfDiameters = len(diameter)
-		first_wire_diameters = diameter[0:lengthOfDiameters//2]
-
-		meanDiameterOfFirstWire = round(sum(first_wire_diameters)/(lengthOfDiameters//2),3)
-
-		console.print("Mean diameter of first wire : ", meanDiameterOfFirstWire, 'cm')
+        console.print("Mean diameter of first wire : ", meanDiameterOfFirstWire, 'cm')
 
 
 d = DiameterOfWire()
